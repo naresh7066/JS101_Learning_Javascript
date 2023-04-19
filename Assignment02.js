d6986ce1c655f19b86f6142443d33b7320e4d14c
Naresh <nareshmore7066@gmail.com>
2023-04-20 02:56:23 +0530

    Day 1 Programs

---
+++ Assignment02.js
@@ -0,0 +1,6 @@
+let Naresh = 10;
+console.log(Naresh);
+Naresh = "Uttam";
+console.log(Naresh);
+Naresh = "Seema";
+console.log(Naresh);
---
+++ Assignment03.js
@@ -0,0 +1,4 @@
+let Name = "Naresh More";
+let Age = 25;
+console.log(Name, Age);
+console.log(typeof(Name), typeof(Age));
